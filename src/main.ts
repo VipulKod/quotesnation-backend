56f3303baed3fff7bc5c086bb67471bf6229f881
Vipul Kodle <machome@Vipuls-MacBook-Air.local>
2023-03-16 22:05:32 +0530

    Added global prefix for calling backend api

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpExceptionFilter } from './httpException.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(3000);
 }
