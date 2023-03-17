96da9b7e457994ed33cc9be8d26cb84440d1d5dd
Vipul Kodle <machome@Vipuls-MacBook-Air.local>
2023-03-17 19:22:26 +0530

    Changes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './httpException.filter';
-import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter());
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api');
-  app.useGlobalFilters(new HttpExceptionFilter());
+  // app.setGlobalPrefix('api');
+  // app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
 }
