9900195b5aff969a5ce716548b5c67d2b04cb1ea
Vipul Kodle <machome@Vipuls-MacBook-Air.local>
2023-03-17 10:44:01 +0530

    Added options handler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Options } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Options('*')
+  handleOptions() {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        'Access-Control-Allow-Headers': 'authorization,content-type',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
