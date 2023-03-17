66b9d3ca2aebd36b4988864562561a122a30e517
Vipul Kodle <machome@Vipuls-MacBook-Air.local>
2023-03-17 09:17:17 +0530

    Added cors interceptor to allow sending cors requests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { cors } from './middleware/cors.middleware';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseInterceptors(cors)
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './httpException.filter';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix('api');
   app.enableCors();
