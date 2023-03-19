1d7e5336671298e220aab17f95145353578e97ed
Vipul Kodle <machome@Vipuls-MacBook-Air.local>
2023-03-19 10:54:59 +0530

    Found the issue for CORS Error and resolved it

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -1,18 +1,11 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    res.setHeader(
-      'Access-Control-Allow-Origin',
-      'https://www.quotesnation.in, https://quotesnation-client.vercel.app',
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization',
-    );
+  use(req: any, res: any, next: () => void) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
   }
 }
